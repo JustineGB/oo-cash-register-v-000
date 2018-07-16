8e2fd8f135ceeefc4630e1b403c10cf018e91d1f
Justine Bricknell <justinegayton@gmail.com>
2018-07-16 11:11:31 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -38,7 +38,7 @@ def apply_discount
 if @discount == 0
     return "There is no discount to apply."
 else
-  @total -= @total * discount.percentage)
+  @total -= @total * (discount/100)
   #new instance var total = instance var total - (instance var total * discount)
   #10 - (10*50%), 10 - 5 = 5
   return "After the discount, the total comes to $#{@total}."
